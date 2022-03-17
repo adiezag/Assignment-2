1d8e825e9dcf86a75816153247e20866bc5bd307
Antonio <idiezag@gmail.com>
2022-03-17 00:06:38 -0400

    Editing script.js to implement addC() function

--- script.js
+++ script.js
@@ -26,12 +26,28 @@ function addR() {
             row.appendChild(col);
         }
         grid.appendChild(row);
+    }
 }
 //Adds a column
 function addC() {
     //alert("Clicked Add Col")
+    let grid = document.getElementById("grid");
     let cols = document.getElementsByTagName("td");
+    let rows = document.getElementsByTagName("tr");
     console.log(cols.length); 
+    if (cols.length === 0) {
+        let row = document.createElement("tr");
+        let col = document.createElement("td");
+        row.appendChild(col);
+        grid.appendChild(row);
+    }else {
+        let numRows = grid.childElementCount;
+        for (var j = 0; j < numRows; j++)
+        {
+            let col = document.createElement("td");
+            rows[j].appendChild(col);
+        }
+    }
 }
 
 //Removes a row
